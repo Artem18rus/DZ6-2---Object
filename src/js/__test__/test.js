9e133b4c5e48b0a290f44f89cdf1128c828c5bdf
Artem18 <urart88@mail.ru>
2022-09-13 20:03:22 +0400

    DZ6-2---Object

---
+++ src/js/__test__/test.js
@@ -0,0 +1,18 @@
+import { character, special, func, result } from '../decision2.js';
+
+test('Function DZ6-2', () => {
+  expect(result).toEqual([
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ]);
+});
---
+++ src/js/decision2.js
@@ -0,0 +1,34 @@
+export const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+export const { special } = character;
+
+export function func() {
+  for (const key in special) {
+    if (!special[key].description) {
+      special[key].description = 'Описание недоступно';
+    }
+  }
+  return special;
+}
+export const result = func();
